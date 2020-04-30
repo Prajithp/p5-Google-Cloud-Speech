35e018218215d6dc69b3015fe2be770b09744eb9
Prajith Ndz <prajithpalakkuda@gmail.com>
2020-04-30 15:10:47 +0530

    bug fixes and version bump

--- bin/transcribe.pl
+++ bin/transcribe.pl
@@ -7,3 +7,12 @@ use Data::Dumper;
 my $speech = Google::Cloud::Speech->new( file => $ARGV[0], secret_file => $ARGV[1] );
 
 print Dumper $speech->syncrecognize->results;
+
+my $operation = $speech->asyncrecognize();
+my $is_done = $operation->is_done;
+
+until($is_done) {
+    if ($is_done = $operation->is_done) {
+        print Dumper $operation->results;
+    }
+}
